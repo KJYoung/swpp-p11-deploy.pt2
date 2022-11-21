c9cd48d6c89b9eacbb6a44a86485ad7e44a3f7c0
gajagajago <gajagajago@naver.com>
2022-11-21 13:46:05 +0000

    add docker be command

---
+++ run_docker_backend.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ABSOLUTE_DB_PATH="/home/ubuntu/swpp-p11-deploy.pt2/backend/db.sqlite3"
+BACKEND_CONTAINER_DB_PATH="/app/db.sqlite3"
+
+sudo docker run -it --rm \
+    --name "backend" \
+    -p 8000:8000 \
+    -v $ABSOLUTE_DB_PATH:$BACKEND_CONTAINER_DB_PATH \
+    backend:latest bash
