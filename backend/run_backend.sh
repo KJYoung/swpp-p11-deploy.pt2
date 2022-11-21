cce98a7bec9e42d92d0467f11d8c936a4d36c618
gajagajago <gajagajago@naver.com>
2022-11-21 03:42:38 +0000

    add backend dockerfile & modify entrypoint

--- backend/run_backend.sh
+++ backend/run_backend.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 # TODO: Write automation script for launching BE app
+python manage.py makemigrations 
+python manage.py migrate
+uwsgi \
+    --module=toh.wsgi:application \
+    --wsgi-file toh/wsgi.py \
+    --http :8000
